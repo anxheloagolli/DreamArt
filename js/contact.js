076928e140ca2a32bb05f1180a2e90d42fb7e012
anxheloagolli <anxheloagolli19@gmail.com>
2024-11-27 17:35:34 +0100

    first commit

---
+++ js/contact.js
@@ -0,0 +1,16 @@
+function Kontakt(event){
+event.preventDefault()
+let emri = document.getElementById("emri").value;
+let mbiemri = document.getElementById("mbiemri").value;
+let email = document.getElementById("email").value;
+let koment = document.getElementById("koment").value;
+let sms = document.getElementById("sms");
+
+if(emri == "" || mbiemri == "" || email == "" || koment == ""){
+sms.textContent = "*Fill all the fields"
+sms.style.color = "red"
+sms.style.fontFamily = "sun-serif"
+}else{
+    sms.textContent = "Thank you" + emri + " " +mbiemri+ "!";
+}
+}
